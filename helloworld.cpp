fd901471c51e1c517e34ffb3917cd0307a27598b
地灵殿 <12257055+DiLingDian@user.noreply.gitee.com>
2023-10-09 20:46:50 +0800

    还是一点点修改

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,4 +7,9 @@ int main()
     cout << "hello world" << endl;
     cout << "你好 你好" << endl;
     cout << "现在是东东哥变帅了" << endl;
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += i;
+    }
 }
